7c08205c30b25a62303221c74bce7155cd22054c
arunkp <arunkp1986@gmail.com>
2024-07-26 04:53:02 +0000

    minor typo correction

--- process_persistence/run_table_3.sh
+++ process_persistence/run_table_3.sh
@@ -9,38 +9,38 @@ echo "Going to copy run scripts"
 
 cd run_scripts
 
-cp *_case3*.sh ../gem5
+cp *_case4*.sh ../gem5
 
-cp *_case3*.c ../gem5
+cp *_case4*.c ../gem5
 
 cd ../gem5
 
 echo "Going to run Kindle simulation for Figure 4 b"
 
 
-gcc run_pt_nvm_case3_eight_512MB.c -o run_pt_nvm_case3_eight_512MB
+gcc run_pt_nvm_case4_eight_512MB.c -o run_pt_nvm_case4_eight_512MB
 
-./run_pt_nvm_case3_eight_512MB $CWD/output/pagetable/nvm/case3/eight_512MB/gemos.out $CWD/output/pagetable/nvm/case3/eight_512MB/gemos.err
+./run_pt_nvm_case4_eight_512MB $CWD/output/pagetable/nvm/case4/eight_512MB/gemos.out $CWD/output/pagetable/nvm/case4/eight_512MB/gemos.err
 
-gcc run_pt_nvm_case3_four_512MB.c -o run_pt_nvm_case3_four_512MB
+gcc run_pt_nvm_case4_four_512MB.c -o run_pt_nvm_case4_four_512MB
 
-./run_pt_nvm_case3_four_512MB $CWD/output/pagetable/nvm/case3/four_512MB/gemos.out $CWD/output/pagetable/nvm/case3/four_512MB/gemos.err
+./run_pt_nvm_case4_four_512MB $CWD/output/pagetable/nvm/case4/four_512MB/gemos.out $CWD/output/pagetable/nvm/case4/four_512MB/gemos.err
 
-gcc run_pt_nvm_case3_two_512MB.c -o run_pt_nvm_case3_two_512MB
+gcc run_pt_nvm_case4_two_512MB.c -o run_pt_nvm_case4_two_512MB
 
-./run_pt_nvm_case3_two_512MB $CWD/output/pagetable/nvm/case3/two_512MB/gemos.out $CWD/output/pagetable/nvm/case3/two_512MB/gemos.err
+./run_pt_nvm_case4_two_512MB $CWD/output/pagetable/nvm/case4/two_512MB/gemos.out $CWD/output/pagetable/nvm/case4/two_512MB/gemos.err
 
-gcc run_pt_rebuild_case3_eight_512MB.c -o run_pt_rebuild_case3_eight_512MB
+gcc run_pt_rebuild_case4_eight_512MB.c -o run_pt_rebuild_case4_eight_512MB
 
-./run_pt_rebuild_case3_eight_512MB $CWD/output/pagetable/rebuild/case3/eight_512MB/gemos.out $CWD/output/pagetable/rebuild/case3/eight_512MB/gemos.err
+./run_pt_rebuild_case4_eight_512MB $CWD/output/pagetable/rebuild/case4/eight_512MB/gemos.out $CWD/output/pagetable/rebuild/case4/eight_512MB/gemos.err
 
-gcc run_pt_rebuild_case3_four_512MB.c -o run_pt_rebuild_case3_four_512MB
+gcc run_pt_rebuild_case4_four_512MB.c -o run_pt_rebuild_case4_four_512MB
 
-./run_pt_rebuild_case3_four_512MB $CWD/output/pagetable/rebuild/case3/four_512MB/gemos.out $CWD/output/pagetable/rebuild/case3/four_512MB/gemos.err
+./run_pt_rebuild_case4_four_512MB $CWD/output/pagetable/rebuild/case4/four_512MB/gemos.out $CWD/output/pagetable/rebuild/case4/four_512MB/gemos.err
 
-gcc run_pt_rebuild_case3_two_512MB.c -o run_pt_rebuild_case3_two_512MB
+gcc run_pt_rebuild_case4_two_512MB.c -o run_pt_rebuild_case4_two_512MB
 
-./run_pt_rebuild_case3_two_512MB $CWD/output/pagetable/rebuild/case3/two_512MB/gemos.out $CWD/output/pagetable/rebuild/case3/two_512MB/gemos.err
+./run_pt_rebuild_case4_two_512MB $CWD/output/pagetable/rebuild/case4/two_512MB/gemos.out $CWD/output/pagetable/rebuild/case4/two_512MB/gemos.err
 
 echo "Going to generate Table 3"
 
